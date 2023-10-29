557e0e9b52d4b8ade4f4c77d5b4103a494b825fc
Vitaliy2773 <vitalii64773@gmail.com>
2023-10-28 13:57:03 -1100

    comit

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,8 +3,10 @@ import css from './Button.module.css';
 
 export default function Button({ onClick }) {
   return (
-    <button onClick={onClick} className={css.Button}>
-      Load more
-    </button>
+    <div className={css.Btn_container}>
+      <button onClick={onClick} className={css.Button}>
+        Load more
+      </button>
+    </div>
   );
 }
